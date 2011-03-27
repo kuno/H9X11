e75b0723effc44c1120cd359fe349fa900aff4b4
kuno <neokuno@gmail.com>
2011-03-27 13:00:10 +0800

    add 3..10 problems

---
+++ 1-10/5.hs
@@ -0,0 +1,9 @@
+-- Problem 5
+-- (*) Reverse a list.
+--
+-- Example in Haskell:
+-- 
+-- Prelude> reverse "A man, a plan, a canal, panama!"
+-- "!amanap ,lanac a ,nalp a ,nam A"
+-- Prelude> reverse [1,2,3,4]
+-- [4,3,2,1]
